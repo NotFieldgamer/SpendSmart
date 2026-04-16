3caa0671f439c30b794051936c9b135f0f99a885
NotFieldgamer <fieldgamer789@gmail.com>
2026-04-16 23:05:36 +0530

    Express v2

--- spendsmart-finance/server/server.js
+++ spendsmart-finance/server/server.js
@@ -26,7 +26,7 @@ app.use(cors({
   credentials: true
 }))
 
-app.options('/*', cors())
+app.options(/.*/, cors())
 
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true }));
